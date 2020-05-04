58359a71727668ba4ec512791d854561dd143386
shellrausch <shellrausch@protonmail.com>
2020-05-04 20:57:22 +0200

    Update tools

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Homebrew all in one setup
 
-Just a small script which installs via Homebrew my most needed applications.
+Just a small script which installs via Homebrew my most needed applications and tools.
 
 ```bash
-chmod +x install.sh
-./install.sh
+chmod +x install.sh && ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ export HOMEBREW_NO_ANALYTICS=1
 
 brew analytics off
 
-brew install zsh zsh-completions
 brew install aria2
-brew install dep
+brew install diff-so-fancy
 brew install go --cross-compile-common
-brew install hashcat
 brew install htop
 brew install nmap
+brew install parallel
 brew install pwgen
 brew install ssh-copy-id
 brew install tree
@@ -20,18 +19,20 @@ brew install unzip
 brew install watch
 brew install wget
 brew install youtube-dl
+brew install zsh zsh-completions
 
+brew cask install adoptopenjdk11
 brew cask install appcleaner
+brew cask install brave-browser
 brew cask install docker
 brew cask install firefox
-brew cask install google-chrome
 brew cask install hex-fiend
+brew cask install intellij-idea-ce
 brew cask install iterm2
 brew cask install macdown
 brew cask install sublime-text
 brew cask install the-unarchiver
-brew cask install transmission
 brew cask install tunnelblick
 brew cask install visual-studio-code
+brew cask install vivaldi
 brew cask install vlc
-
